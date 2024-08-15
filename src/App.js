166b522d5020b53cc1a406fbafbe5539dc3fed3d
Shruti-jha6 <shruti119btcsai22@igdtuw.ac.in>
2024-08-15 17:49:03 +0530

    chat answer format

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+// import {useStates } from 'react'
 
 function App() {
+  // const [ques, setQues]
+  async function generateans() {
+    console.log('Generating');
+    try {
+      const response = await axios({
+        url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyDML4qGZ0Wi2rc7n_s8bcKAWnmgjJk5uew",
+        method: "POST",
+        data: {
+          "contents": [{"parts": [{"text": "how many sections are there in the company law 2013"}]}],
+        },
+      });
+      const generatedText = response.data.candidates[0].content.parts[0].text;
+    console.log(generatedText);
+    } catch (error) {
+      console.error('Error generating response:', error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>Kya hukum mere aaka!</h1>
+      {/* <textarea value={} cols> */}
+
+      {/* </textarea> */}
+
+      <button onClick={generateans}>Generate</button>
     </div>
   );
 }
